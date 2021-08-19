0dd8d18374d572f66ef68222864ae9388de919a2
beggart <b.eggart@gmail.com>
2021-08-19 17:23:33 +0200

    initial commit

---
+++ server.R
@@ -0,0 +1,43 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(rsconnect)
+# Select columns to be used in the analysis
+
+beer_data <- data.table::fread(input = "https://www.opengov-muenchen.de/dataset/8d6c8251-7956-4f92-8c96-f79106aab828/resource/e0f664cf-6dd9-4743-bd2b-81a8b18bd1d2/download/oktoberfestgesamt19852019.csv"
+                               , na.strings="?"
+)
+
+# Define server logic required to draw a plot
+shinyServer(function(input, output) {
+  output$distPlot <- renderPlot({
+    # Select diamonds depending of user input
+    beer <- filter(beer_data, grepl(input$conprice, ), grepl(input$col, color), grepl(input$clar, clarity))
+    # build linear regression model
+    fit <- lm( price~carat, diam)
+    # predicts the price
+    pred <- predict(fit, newdata = data.frame(carat = input$car,
+                                              cut = input$cut,
+                                              color = input$col,
+                                              clarity = input$clar))
+    # Draw the plot using ggplot2
+    plot <- ggplot(data=diam, aes(x=carat, y = price))+
+      geom_point(aes(color = cut), alpha = 0.3)+
+      geom_smooth(method = "lm")+
+      geom_vline(xintercept = input$car, color = "red")+
+      geom_hline(yintercept = pred, color = "green")
+    plot
+  })
+  output$result <- renderText({
+    # Renders the text for the prediction below the graph
+    diam <- filter(diamonds, grepl(input$cut, cut), grepl(input$col, color), grepl(input$clar, clarity))
+    fit <- lm( price~carat, diam)
+    pred <- predict(fit, newdata = data.frame(carat = input$car,
+                                              cut = input$cut,
+                                              color = input$col,
+                                              clarity = input$clar))
+    res <- paste(round(pred, digits = 1.5),"$" )
+    res
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,30 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Price predictor for Beer at the Oktoberfest"),
+  
+  # Sidebar with options selectors
+  sidebarLayout(
+    sidebarPanel(
+      helpText("This application is a predictor for the price of beer based on historical beer prices."),
+      h3(helpText("Select:")),
+      numericInput("year", label = h4("Year to predict"), step = 2009, value = 2100),
+      selectInput("type", label = h4("Type"),
+                  choices = list("Beer" = "Beer", "Chicken" = "Chicken")),
+      selectInput("conprice", label = h4("Consumption/Price"),
+                  choices = list("Consumption" = "Consumption", "Price"="Price")),
+      selectInput("Duration", label = h4("Duration"),
+                  choices = list("Unknown" = "*", "16" = "16", "17" = "17", "18"="18"))
+    ),
+    
+    # Show a plot with diamonds and regression line
+    mainPanel(
+      plotOutput("distPlot"),
+      h4("Predicted value of this diamond is:"),
+      h3(textOutput("result"))
+    )
+  )
+))
